84e236cd21de6656ac9cdc06205e9aee8942c305
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-07-20 12:23:26 +0530

    setup app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constands";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app, port } from "./app.js";
 
+
+dotenv.config({path: "./env"})  // this is imp without this env will not load
+
+
+connectDB()  // when db is connected it return a promiss
+.then( () => {
+    app.on("ERROR", (err) => {
+        console.log("ERROR", error)
+        throw error
+    })
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`)
+    })
+})
+.catch((err) => {
+    console.log(`MONGO db connection FAILED !!!`, err)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import express from "express"
 const app = express()
-
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
@@ -11,14 +48,12 @@ const app = express()
             console.log("ERROR", error)
             throw error
         })
-
         app.listen(process.env.PORT,() => {
             console.log(`app is listening on port ${process.env.PORT}`);
         })
-
         
     } catch (error) {
         console.log("ERROR", error);
         throw error
     }
-} )()
+} )()*/
