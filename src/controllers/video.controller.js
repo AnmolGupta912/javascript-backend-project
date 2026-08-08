999596941118d0fc6424e69f2397b3bc3c8711d1
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-08-09 01:11:02 +0530

    add: implement video update, delete, and publish status toggle functionalities

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,9 +1,14 @@
 import { ApiError } from "../utils/ApiError.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
-// import { uploadFileOnCloudinary } from "../utils/cloudinary.js"
 import {uploadFileOnCloudinary} from "../utils/cloudinary.js"
 import { Video } from "../models/video.model.js"
 import { ApiRespone } from "../utils/ApiRespone.js"
+
+
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    //TODO: get all videos based on query, sort, pagination
+})
 
 
 const publishAVideo = asyncHandler(async(req , res) => {
@@ -75,8 +80,88 @@ const getVideoById = asyncHandler( async(req, res) => {
 
 })
 
+const updateVideo = asyncHandler(async(req, res) => {
+    //TODO: update video details like title, description, thumbnail
+    // videoId <= req.params
+    // upload thumbnail from local to cloudinay as well check 
+    // get title and description <= req.body
+    // findbyidAndUpdate then send res
+
+    const { videoId } = req.params
+
+    const thumbnailLocalPath = req.file?.thumbnail?.path
+
+    if (!thumbnailLocalPath) {
+        throw new ApiError(400, "thumbnail is missing!!!")
+    }
+
+    const thumbnail = await uploadFileOnCloudinary(thumbnailLocalPath)
+     if (!thumbnail.url) {
+            throw new ApiError(500, "Can't get the thumbnail url!!!")
+        }
+
+    const video = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            title,
+            description,
+            thumbnail: thumbnail?.url
+        },
+        {
+            new: true
+        })
+
+    if (!video) {
+        throw new ApiError(400, "Can't update the video!!!");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiRespone(200, video, "Video updated successfully!!!"))
+
+})
+
+const deleteVideo = asyncHandler(async(req, res) => {
+    //TODO: delete video
+    // videoId <= req.params
+    // findbyidAndDelete then send res
+
+    const { videoId } = req.params
+
+    const video = await Video.findByIdAndDelete(videoId)
+    if (!video) {
+        throw new ApiError(400, "Can't delete the video!!!");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiRespone(200, video, "Video deleted successfully!!!"))
+
+})
+
+const togglePublishStatus = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+
+    const video = await Video.findById(videoId)
+    if (!video) {
+        throw new ApiError(400, "Can't find the video!!!");
+    }
+
+    video.isPublished = !video.isPublished
+    await video.save({ validateBeforeSave: false })
+
+    return res
+    .status(200)
+    .json(new ApiRespone(200, video, "Video publish status updated successfully!!!"))
+
+})
+
 
 export {
     publishAVideo,
-    getVideoById
+    getVideoById,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus,
+    
 }
