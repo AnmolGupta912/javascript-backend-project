614297c81e248fdf96a4617bdf1d527c0f5611ff
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-08-15 15:40:09 +0530

    feat: implement comment management functions including fetching, adding, updating, and deleting comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,11 +9,30 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: videoId
+            }
+        },
+        {
+            $limit: limit*page
+        }
+    ])
+
+    if (!comments) {
+        throw new ApiError(400, "problem while fetching comments!!!")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiRespone(200, comments, "Successfully fetched the comments!!!"))
+
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
-    const {userId} = req.userId
+    const {userId} = req.user?._id
     const {content} = req.body
 
     const comment = await Comment.create({
@@ -33,10 +52,34 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params 
+    const {content} = req.body
+    
+    const comment = await Comment.findByIdAndUpdate(commentId, {content}, {new: true})
+
+    if(!comment) {
+        throw new ApiError(400, "problem while updating comment!!!")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiRespone(200, comment, "Successfully updated the comment!!!"))
+
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+
+    const comment = await Comment.findByIdAndDelete(commentId)
+
+    if(!comment) {
+        throw new ApiError(400, "problem while deleting comment!!!")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiRespone(200, comment, "Successfully deleted the comment!!!"))
 })
 
 export {
